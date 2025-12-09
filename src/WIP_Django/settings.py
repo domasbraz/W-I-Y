c58a220d515c1abbb7c36b9232dfcae59e1cfd28
domasbraz <domasbraz@gmail.com>
2025-12-09 16:11:18 +0000

    fixed browser issue with development

--- src/WIP_Django/settings.py
+++ src/WIP_Django/settings.py
@@ -126,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Browser may attempt to optimise load times by caching static files, this bypasses it
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
